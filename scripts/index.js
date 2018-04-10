b5f2d82b144ef146373374c7d47139e2ba7074ec
neillsom <neillsom@mail.com>
2018-04-10 12:17:07 -0700

    pushed back

--- scripts/index.js
+++ scripts/index.js
@@ -1,181 +1,181 @@
 'use strict';
 // mockData for testing purposes only
 const mockData = {
-  'kind': 'youtube#searchListResponse',
-  'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/oEv89t51Sd8ORH2C2kRAmfcSnhA"',
-  'nextPageToken': 'CAUQAA',
-  'regionCode': 'US',
-  'pageInfo': {
-    'totalResults': 1000000,
-    'resultsPerPage': 5
-  },
-  'items': [
-    {
-      'kind': 'youtube#searchResult',
-      'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/_MkRnwqaJXMWSxS07gHgGxwatjc"',
-      'id': {
-        'kind': 'youtube#video',
-        'videoId': 'pAsN4lSa7O0'
-      },
-      'snippet': {
-        'publishedAt': '2017-09-08T10:00:03.000Z',
-        'channelId': 'UC9nxc-xiH1AnL9RGstfgurg',
-        'title': 'Blake Shelton - "I\'ll Name The Dogs" (Official Music Video)',
-        'description': 'NEW ALBUM "Texoma Shore" available NOW: https://BlakeShelton.lnk.to/texomashore See Blake on the "Country Music Freaks" Tour with Brett Eldredge and Carly Pearce! Tickets on sale now. Visit...',
-        'thumbnails': {
-          'default': {
-            'url': 'https://i.ytimg.com/vi/pAsN4lSa7O0/default.jpg',
-            'width': 120,
-            'height': 90
-          },
-          'medium': {
-            'url': 'https://i.ytimg.com/vi/pAsN4lSa7O0/mqdefault.jpg',
-            'width': 320,
-            'height': 180
-          },
-          'high': {
-            'url': 'https://i.ytimg.com/vi/pAsN4lSa7O0/hqdefault.jpg',
-            'width': 480,
-            'height': 360
-          }
-        },
-        'channelTitle': 'Blake Shelton',
-        'liveBroadcastContent': 'none'
-      }
+    'kind': 'youtube#searchListResponse',
+    'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/oEv89t51Sd8ORH2C2kRAmfcSnhA"',
+    'nextPageToken': 'CAUQAA',
+    'regionCode': 'US',
+    'pageInfo': {
+        'totalResults': 1000000,
+        'resultsPerPage': 5
     },
-    {
-      'kind': 'youtube#searchResult',
-      'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/UbYHD7HGUye1kPQMzZ8JlNRamik"',
-      'id': {
-        'kind': 'youtube#video',
-        'videoId': '4A3tcUUb6M4'
-      },
-      'snippet': {
-        'publishedAt': '2017-09-09T10:32:28.000Z',
-        'channelId': 'UC3ZhVuv7S48bAVJsYGMC_bQ',
-        'title': 'Blake Shelton - I\'ll Name The Dogs (LYRICS)',
-        'description': 'Blake Shelton I\'ll Name The Dogs lyrics Blake shelton i\'ll name the dogs lyrics blake shelton i will name the dogs Lyrics.',
-        'thumbnails': {
-          'default': {
-            'url': 'https://i.ytimg.com/vi/4A3tcUUb6M4/default.jpg',
-            'width': 120,
-            'height': 90
-          },
-          'medium': {
-            'url': 'https://i.ytimg.com/vi/4A3tcUUb6M4/mqdefault.jpg',
-            'width': 320,
-            'height': 180
-          },
-          'high': {
-            'url': 'https://i.ytimg.com/vi/4A3tcUUb6M4/hqdefault.jpg',
-            'width': 480,
-            'height': 360
-          }
+    'items': [
+        {
+            'kind': 'youtube#searchResult',
+            'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/_MkRnwqaJXMWSxS07gHgGxwatjc"',
+            'id': {
+                'kind': 'youtube#video',
+                'videoId': 'pAsN4lSa7O0'
+            },
+            'snippet': {
+                'publishedAt': '2017-09-08T10:00:03.000Z',
+                'channelId': 'UC9nxc-xiH1AnL9RGstfgurg',
+                'title': 'Blake Shelton - "I\'ll Name The Dogs" (Official Music Video)',
+                'description': 'NEW ALBUM "Texoma Shore" available NOW: https://BlakeShelton.lnk.to/texomashore See Blake on the "Country Music Freaks" Tour with Brett Eldredge and Carly Pearce! Tickets on sale now. Visit...',
+                'thumbnails': {
+                    'default': {
+                        'url': 'https://i.ytimg.com/vi/pAsN4lSa7O0/default.jpg',
+                        'width': 120,
+                        'height': 90
+                    },
+                    'medium': {
+                        'url': 'https://i.ytimg.com/vi/pAsN4lSa7O0/mqdefault.jpg',
+                        'width': 320,
+                        'height': 180
+                    },
+                    'high': {
+                        'url': 'https://i.ytimg.com/vi/pAsN4lSa7O0/hqdefault.jpg',
+                        'width': 480,
+                        'height': 360
+                    }
+                },
+                'channelTitle': 'Blake Shelton',
+                'liveBroadcastContent': 'none'
+            }
         },
-        'channelTitle': 'Music Lyrics',
-        'liveBroadcastContent': 'none'
-      }
-    },
-    {
-      'kind': 'youtube#searchResult',
-      'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/LX5jpXx9Zq8byA0OggXWZ5Lz9d4"',
-      'id': {
-        'kind': 'youtube#video',
-        'videoId': 'X_mbNE5UzHI'
-      },
-      'snippet': {
-        'publishedAt': '2015-04-04T14:20:55.000Z',
-        'channelId': 'UCSY5Ca_55GqRJfWE3lu1cEw',
-        'title': 'dogs called by name one at a time',
-        'description': 'These patient dogs have all learnt to wait until their name is called before they eat!',
-        'thumbnails': {
-          'default': {
-            'url': 'https://i.ytimg.com/vi/X_mbNE5UzHI/default.jpg',
-            'width': 120,
-            'height': 90
-          },
-          'medium': {
-            'url': 'https://i.ytimg.com/vi/X_mbNE5UzHI/mqdefault.jpg',
-            'width': 320,
-            'height': 180
-          },
-          'high': {
-            'url': 'https://i.ytimg.com/vi/X_mbNE5UzHI/hqdefault.jpg',
-            'width': 480,
-            'height': 360
-          }
+        {
+            'kind': 'youtube#searchResult',
+            'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/UbYHD7HGUye1kPQMzZ8JlNRamik"',
+            'id': {
+                'kind': 'youtube#video',
+                'videoId': '4A3tcUUb6M4'
+            },
+            'snippet': {
+                'publishedAt': '2017-09-09T10:32:28.000Z',
+                'channelId': 'UC3ZhVuv7S48bAVJsYGMC_bQ',
+                'title': 'Blake Shelton - I\'ll Name The Dogs (LYRICS)',
+                'description': 'Blake Shelton I\'ll Name The Dogs lyrics Blake shelton i\'ll name the dogs lyrics blake shelton i will name the dogs Lyrics.',
+                'thumbnails': {
+                    'default': {
+                        'url': 'https://i.ytimg.com/vi/4A3tcUUb6M4/default.jpg',
+                        'width': 120,
+                        'height': 90
+                    },
+                    'medium': {
+                        'url': 'https://i.ytimg.com/vi/4A3tcUUb6M4/mqdefault.jpg',
+                        'width': 320,
+                        'height': 180
+                    },
+                    'high': {
+                        'url': 'https://i.ytimg.com/vi/4A3tcUUb6M4/hqdefault.jpg',
+                        'width': 480,
+                        'height': 360
+                    }
+                },
+                'channelTitle': 'Music Lyrics',
+                'liveBroadcastContent': 'none'
+            }
         },
-        'channelTitle': 'wonderfull World',
-        'liveBroadcastContent': 'none'
-      }
-    },
-    {
-      'kind': 'youtube#searchResult',
-      'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/FpT3xFzdHQdZnG0XWpfHKURn1yw"',
-      'id': {
-        'kind': 'youtube#video',
-        'videoId': 'DZDZANu33rY'
-      },
-      'snippet': {
-        'publishedAt': '2017-10-06T13:00:02.000Z',
-        'channelId': 'UCINb0wqPz-A0dV9nARjJlOQ',
-        'title': 'SMART Dog Knows Toys By Name | The Dodo',
-        'description': 'SMART Dog Knows All Her Toys By Name | This super smart dog knows her toys by name — but can she remember them ALL? Special thanks to Ori Lieberman for this amazing video! For more of this...',
-        'thumbnails': {
-          'default': {
-            'url': 'https://i.ytimg.com/vi/DZDZANu33rY/default.jpg',
-            'width': 120,
-            'height': 90
-          },
-          'medium': {
-            'url': 'https://i.ytimg.com/vi/DZDZANu33rY/mqdefault.jpg',
-            'width': 320,
-            'height': 180
-          },
-          'high': {
-            'url': 'https://i.ytimg.com/vi/DZDZANu33rY/hqdefault.jpg',
-            'width': 480,
-            'height': 360
-          }
+        {
+            'kind': 'youtube#searchResult',
+            'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/LX5jpXx9Zq8byA0OggXWZ5Lz9d4"',
+            'id': {
+                'kind': 'youtube#video',
+                'videoId': 'X_mbNE5UzHI'
+            },
+            'snippet': {
+                'publishedAt': '2015-04-04T14:20:55.000Z',
+                'channelId': 'UCSY5Ca_55GqRJfWE3lu1cEw',
+                'title': 'dogs called by name one at a time',
+                'description': 'These patient dogs have all learnt to wait until their name is called before they eat!',
+                'thumbnails': {
+                    'default': {
+                        'url': 'https://i.ytimg.com/vi/X_mbNE5UzHI/default.jpg',
+                        'width': 120,
+                        'height': 90
+                    },
+                    'medium': {
+                        'url': 'https://i.ytimg.com/vi/X_mbNE5UzHI/mqdefault.jpg',
+                        'width': 320,
+                        'height': 180
+                    },
+                    'high': {
+                        'url': 'https://i.ytimg.com/vi/X_mbNE5UzHI/hqdefault.jpg',
+                        'width': 480,
+                        'height': 360
+                    }
+                },
+                'channelTitle': 'wonderfull World',
+                'liveBroadcastContent': 'none'
+            }
         },
-        'channelTitle': 'The Dodo',
-        'liveBroadcastContent': 'none'
-      }
-    },
-    {
-      'kind': 'youtube#searchResult',
-      'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/jDPAii3DzqTksIqmS593cNmrm_o"',
-      'id': {
-        'kind': 'youtube#video',
-        'videoId': 'V0aydZb5eA0'
-      },
-      'snippet': {
-        'publishedAt': '2018-03-15T10:18:44.000Z',
-        'channelId': 'UC7xz_YipuZtypOuVALNzeog',
-        'title': 'In the Name of Jesus (feat. October London)',
-        'description': 'Provided to YouTube by Sony Music Entertainment In the Name of Jesus (feat. October London) · Snoop Dogg · October London Snoop Dogg Presents Bible of Love ℗ An RCA Inspiration release....',
-        'thumbnails': {
-          'default': {
-            'url': 'https://i.ytimg.com/vi/V0aydZb5eA0/default.jpg',
-            'width': 120,
-            'height': 90
-          },
-          'medium': {
-            'url': 'https://i.ytimg.com/vi/V0aydZb5eA0/mqdefault.jpg',
-            'width': 320,
-            'height': 180
-          },
-          'high': {
-            'url': 'https://i.ytimg.com/vi/V0aydZb5eA0/hqdefault.jpg',
-            'width': 480,
-            'height': 360
-          }
+        {
+            'kind': 'youtube#searchResult',
+            'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/FpT3xFzdHQdZnG0XWpfHKURn1yw"',
+            'id': {
+                'kind': 'youtube#video',
+                'videoId': 'DZDZANu33rY'
+            },
+            'snippet': {
+                'publishedAt': '2017-10-06T13:00:02.000Z',
+                'channelId': 'UCINb0wqPz-A0dV9nARjJlOQ',
+                'title': 'SMART Dog Knows Toys By Name | The Dodo',
+                'description': 'SMART Dog Knows All Her Toys By Name | This super smart dog knows her toys by name — but can she remember them ALL? Special thanks to Ori Lieberman for this amazing video! For more of this...',
+                'thumbnails': {
+                    'default': {
+                        'url': 'https://i.ytimg.com/vi/DZDZANu33rY/default.jpg',
+                        'width': 120,
+                        'height': 90
+                    },
+                    'medium': {
+                        'url': 'https://i.ytimg.com/vi/DZDZANu33rY/mqdefault.jpg',
+                        'width': 320,
+                        'height': 180
+                    },
+                    'high': {
+                        'url': 'https://i.ytimg.com/vi/DZDZANu33rY/hqdefault.jpg',
+                        'width': 480,
+                        'height': 360
+                    }
+                },
+                'channelTitle': 'The Dodo',
+                'liveBroadcastContent': 'none'
+            }
         },
-        'channelTitle': 'Snoop Dogg - Topic',
-        'liveBroadcastContent': 'none'
-      }
-    }
-  ]
+        {
+            'kind': 'youtube#searchResult',
+            'etag': '"RmznBCICv9YtgWaaa_nWDIH1_GM/jDPAii3DzqTksIqmS593cNmrm_o"',
+            'id': {
+                'kind': 'youtube#video',
+                'videoId': 'V0aydZb5eA0'
+            },
+            'snippet': {
+                'publishedAt': '2018-03-15T10:18:44.000Z',
+                'channelId': 'UC7xz_YipuZtypOuVALNzeog',
+                'title': 'In the Name of Jesus (feat. October London)',
+                'description': 'Provided to YouTube by Sony Music Entertainment In the Name of Jesus (feat. October London) · Snoop Dogg · October London Snoop Dogg Presents Bible of Love ℗ An RCA Inspiration release....',
+                'thumbnails': {
+                    'default': {
+                        'url': 'https://i.ytimg.com/vi/V0aydZb5eA0/default.jpg',
+                        'width': 120,
+                        'height': 90
+                    },
+                    'medium': {
+                        'url': 'https://i.ytimg.com/vi/V0aydZb5eA0/mqdefault.jpg',
+                        'width': 320,
+                        'height': 180
+                    },
+                    'high': {
+                        'url': 'https://i.ytimg.com/vi/V0aydZb5eA0/hqdefault.jpg',
+                        'width': 480,
+                        'height': 360
+                    }
+                },
+                'channelTitle': 'Snoop Dogg - Topic',
+                'liveBroadcastContent': 'none'
+            }
+        }
+    ]
 };
 
 // DONE!
@@ -194,7 +194,7 @@ const API_KEY = 'AIzaSyBFz0lxQL-8arxYWSUs8rmCQSLM6jqAwrI';
 
 */
 const store = {
-  videos: []
+    videos: []
 };
 
 // TASK: Add the Youtube Search API Base URL here:
@@ -210,12 +210,12 @@ const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 // TEST IT! Execute this function and console log the results inside the callback.
 // DONE!
 const fetchVideos = function (searchTerm, callback) {
-  const query = {
-    q: `${searchTerm} in:name`,
-    key: API_KEY,
-    part: 'snippet'
-  };
-  $.getJSON(BASE_URL, query, callback);
+    const query = {
+        q: `${searchTerm} in:name`,
+        key: API_KEY,
+        part: 'snippet'
+    };
+    $.getJSON(BASE_URL, query, callback);
 };
 
 
@@ -228,12 +228,12 @@ const fetchVideos = function (searchTerm, callback) {
 // TEST IT! Grab an example API response and send it into the function - make sure
 // you get back the object you want.
 const decorateResponse = function (response) {
-  return response.items.map(item => { 
-    const id = item.id.videoId; 
-    const title = item.snippet.title; 
-    const thumbnail = item.snippet.thumbnails.default.url; 
-    return { id, title, thumbnail, }; 
-  });
+    return response.items.map(item => { 
+        const id = item.id.videoId; 
+        const title = item.snippet.title; 
+        const thumbnail = item.snippet.thumbnails.default.url; 
+        return { id, title, thumbnail, }; 
+    });
 };
 
 // TASK:
@@ -241,7 +241,7 @@ const decorateResponse = function (response) {
 // 2. Using the object, return an HTML string containing all the expected data
 // TEST IT!
 const generateVideoItemHtml = function (video) {
-  return `
+    return `
     <li> <img src="${video.thumbnail}"> <p>${video.title}</p> </li> `;
 };
 
@@ -250,7 +250,7 @@ const generateVideoItemHtml = function (video) {
 // objects and sets the array as the value held in store.items
 // TEST IT!
 const addVideosToStore = function (videos) {
-  store.videos = videos;
+    store.videos = videos;
 };
 
 // TASK:
@@ -279,6 +279,6 @@ const handleFormSubmit = function () {
 
 // When DOM is ready:
 $(function () {
-  // TASK:
-  // 1. Run `handleFormSubmit` to bind the event listener to the DOM
+    // TASK:
+    // 1. Run `handleFormSubmit` to bind the event listener to the DOM
 });
